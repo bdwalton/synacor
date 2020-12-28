f8b1bf3b4450e5a15d6f4e9477a6dbcb5df7a4ce
Ben Walton <bdwalton@gmail.com>
2020-12-28 16:18:40 +0000

    Second code. Passing initialization.
    Signed-off-by: Ben Walton <bdwalton@gmail.com>

--- synacor/synacor.go
+++ synacor/synacor.go
@@ -1,21 +1,23 @@
 package synacor
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
+	"os"
 )
 
 const (
-	NREGS     = 8 // Number of registers
-	MAX_15BIT = 32767
-	MAX_REG   = MAX_15BIT + 8 // indirect register references
+	NREGS          = 8     // Number of registers
+	MAX_15BIT      = 32767 // Values are 0..MAX_15BIT
+	OVERFLOW_15BIT = 32768
+	MAX_REG        = MAX_15BIT + 8 // indirect register references
 )
 
 // CPU states
 const (
-	RUNNING = iota
-	HALTED
-	ERROR
+	RUNNING = iota // Default. Next instruction pointed to by program counter
+	HALTED         // Normal halt.
+	ERROR          // Halted in error state.
 )
 
 // Instruction names
@@ -69,9 +71,46 @@ var opsToString map[int]string = map[int]string{
 	NOOP: "NOOP",
 }
 
+// The number of arguments expected for each OP.
+var argsForOp map[int]uint16 = map[int]uint16{
+	HALT: 0,
+	SET:  2,
+	PUSH: 1,
+	POP:  1,
+	EQ:   3,
+	GT:   3,
+	JMP:  1,
+	JT:   2,
+	JF:   2,
+	ADD:  3,
+	MULT: 3,
+	MOD:  3,
+	AND:  3,
+	OR:   3,
+	NOT:  2,
+	RMEM: 2,
+	WMEM: 2,
+	CALL: 1,
+	RET:  0,
+	OUT:  1,
+	IN:   1,
+	NOOP: 0,
+}
+
+func isReg(arg uint16) bool {
+	return MAX_15BIT < arg && arg <= MAX_REG
+}
+
+func isValue(arg uint16) bool {
+	return arg <= MAX_15BIT
+}
+
+func decipherReg(arg uint16) uint16 {
+	return arg - MAX_15BIT - 1
+}
+
 type Stack struct {
 	data []uint16
-	len  int
 }
 
 func NewStack() *Stack {
@@ -80,29 +119,41 @@ func NewStack() *Stack {
 
 func (s *Stack) Push(v uint16) {
 	s.data = append(s.data, v)
-	s.len += 1
 }
 
-func (s *Stack) Pop() uint16 {
-	v := s.data[s.len-1]
-	s.data = s.data[:s.len-2]
-	s.len -= 1
-	return v
+func (s *Stack) IsEmpty() bool {
+	return len(s.data) == 0
+}
+
+func (s *Stack) Pop() (uint16, bool) {
+	if s.IsEmpty() {
+		return 0, false
+	}
+
+	idx := len(s.data) - 1
+	v := s.data[idx]
+	s.data = s.data[:idx]
+
+	return v, true
 }
 
 type Machine struct {
-	memory []uint16
-	regs   []uint16
-	pc     uint16 // program counter
-	stack  *Stack
-	state  int
+	memory       []uint16
+	regs         []uint16
+	pc           uint16 // program counter
+	stack        *Stack
+	state        int
+	input        *bufio.Reader
+	unused_input []uint16 // Available input
 }
 
 func NewMachine(prog []uint16) *Machine {
 	m := &Machine{
-		memory: make([]uint16, 32768), // 15-bits
-		regs:   make([]uint16, NREGS, NREGS),
-		stack:  NewStack(),
+		memory:       make([]uint16, 32768), // 15-bits
+		regs:         make([]uint16, NREGS, NREGS),
+		stack:        NewStack(),
+		input:        bufio.NewReader(os.Stdin),
+		unused_input: make([]uint16, 0),
 	}
 
 	copy(m.memory, prog)
@@ -111,7 +162,7 @@ func NewMachine(prog []uint16) *Machine {
 }
 
 func (m *Machine) Halted() bool {
-	return m.state == HALTED
+	return m.state != RUNNING
 }
 
 func (m *Machine) Run() {
@@ -120,57 +171,39 @@ func (m *Machine) Run() {
 	}
 }
 
-func (m *Machine) maybeIndirectArgs(arg uint16) (uint16, error) {
-	if arg < MAX_15BIT {
-		return arg, nil
+func (m *Machine) readArg(arg uint16) uint16 {
+	if isValue(arg) {
+		return arg
 	}
 
-	if arg <= MAX_REG {
-		return m.regs[MAX_REG-arg], nil
+	if isReg(arg) {
+		return m.regs[decipherReg(arg)]
 	}
 
-	return 0, errors.New("Invalid argument")
+	m.Error(fmt.Sprintf("Invalid Argument '%d'.", arg))
+	return 0
 }
 
 func (m *Machine) getArgs(op uint16) []uint16 {
-	args := make([]uint16, 0)
-	var n int
-	switch op {
-	case IN, OUT, JMP:
-		n = 1
-	case JT, JF, SET:
-		n = 2
+	if n := argsForOp[int(op)]; n > 0 {
+		return m.memory[m.pc+1 : m.pc+1+n]
 	}
 
-	for i := 1; i <= n; i++ {
-		args = append(args, m.memory[m.pc+uint16(i)])
-	}
-
-	return args
+	return []uint16{}
 }
 
-func (m *Machine) updateProgramCounter(op uint16) {
-	var n uint16 // Number of arguments for operand
-
-	switch op {
-	case HALT:
-		n = 0
-	case NOOP:
-		n = 0
-	case PUSH, POP, OUT, IN:
-		n = 1
-	case SET, JT, JF:
-		n = 2
-	}
-
-	m.pc += 1 + n
+// Move over the OP and the number of args for the OP
+func (m *Machine) nextProgramCounter(op uint16) uint16 {
+	return m.pc + 1 + argsForOp[int(op)]
 }
 
+// Log error and halt machine.
 func (m *Machine) Error(msg string) {
 	fmt.Println(msg)
-	m.Halt()
+	m.state = ERROR
 }
 
+// Halt machine.
 func (m *Machine) Halt() {
 	m.state = HALTED
 }
@@ -182,13 +215,148 @@ func (m *Machine) Step() {
 	switch op {
 	case HALT:
 		m.Halt()
+		return
+	case SET:
+		m.regs[decipherReg(args[0])] = m.readArg(args[1])
+	case PUSH:
+		m.stack.Push(m.readArg(args[0]))
+	case POP:
+		v, ok := m.stack.Pop()
+		if !ok {
+			m.Error("Popped an empty stack.")
+			return
+		}
+
+		if isReg(args[0]) {
+			m.regs[decipherReg(args[0])] = v
+		} else {
+			m.memory[args[0]] = v
+		}
+	case EQ:
+		b, c := m.readArg(args[1]), m.readArg(args[2])
+		var eq uint16
+		if b == c {
+			eq = 1
+		}
+		if isReg(args[0]) {
+			m.regs[decipherReg(args[0])] = eq
+		} else {
+			m.memory[args[0]] = eq
+		}
+	case GT:
+		b, c := m.readArg(args[1]), m.readArg(args[2])
+		var gt uint16
+		if b > c {
+			gt = 1
+		}
+		if isReg(args[0]) {
+			m.regs[decipherReg(args[0])] = gt
+		} else {
+			m.memory[args[0]] = gt
+		}
+	case JMP:
+		m.pc = m.readArg(args[0])
+		return
+	case JT:
+		if m.readArg(args[0]) != 0 {
+			m.pc = m.readArg(args[1])
+			return
+		}
+	case JF:
+		if m.readArg(args[0]) == 0 {
+			m.pc = m.readArg(args[1])
+			return
+		}
+	case ADD:
+		b, c := m.readArg(args[1]), m.readArg(args[2])
+		a := (b + c) % OVERFLOW_15BIT
+
+		if isReg(args[0]) {
+			m.regs[decipherReg(args[0])] = a
+		} else {
+			m.memory[args[0]] = a
+		}
+	case MULT:
+		b, c := m.readArg(args[1]), m.readArg(args[2])
+		a := (b * c) % OVERFLOW_15BIT
+
+		if isReg(args[0]) {
+			m.regs[decipherReg(args[0])] = a
+		} else {
+			m.memory[args[0]] = a
+		}
+	case MOD:
+		b, c := m.readArg(args[1]), m.readArg(args[2])
+		a := b % c
+
+		if isReg(args[0]) {
+			m.regs[decipherReg(args[0])] = a
+		} else {
+			m.memory[args[0]] = a
+		}
+	case AND:
+		b, c := m.readArg(args[1]), m.readArg(args[2])
+		a := b & c
+
+		if isReg(args[0]) {
+			m.regs[decipherReg(args[0])] = a
+		} else {
+			m.memory[args[0]] = a
+		}
+	case OR:
+		b, c := m.readArg(args[1]), m.readArg(args[2])
+		a := b | c
+
+		if isReg(args[0]) {
+			m.regs[decipherReg(args[0])] = a
+		} else {
+			m.memory[args[0]] = a
+		}
+	case NOT:
+		b := m.readArg(args[1])
+		a := (b ^ MAX_15BIT)
+		if isReg(args[0]) {
+			m.regs[decipherReg(args[0])] = a
+		} else {
+			m.memory[args[0]] = a
+		}
+	case RMEM:
+		m.regs[decipherReg(args[0])] = m.memory[m.readArg(args[1])]
+	case WMEM:
+		m.memory[m.readArg(args[0])] = m.readArg(args[1])
+	case CALL:
+		m.stack.Push(m.nextProgramCounter(op))
+		m.pc = m.readArg(args[0])
+		return
+	case RET:
+		if npc, ok := m.stack.Pop(); ok {
+			m.pc = npc
+		} else {
+			m.Error("Popped an empty stack.")
+		}
+		return
 	case OUT:
-		fmt.Printf("%c", args[0])
-		m.updateProgramCounter(op)
+		fmt.Printf("%c", m.readArg(args[0]))
+	case IN:
+		if len(m.unused_input) == 0 {
+			fmt.Printf("Input: ")
+			input, _ := m.input.ReadString('\n')
+			for _, c := range input {
+				m.unused_input = append(m.unused_input, uint16(c))
+			}
+		}
+
+		if isReg(args[0]) {
+			m.regs[decipherReg(args[0])] = m.unused_input[0]
+		} else {
+			m.memory[m.readArg(args[0])] = m.unused_input[0]
+		}
+
+		m.unused_input = m.unused_input[1:]
 	case NOOP:
-		m.updateProgramCounter(op)
 	default:
-		fmt.Printf("UNIMPLEMENTED INSTRUCTION: %q\n", opsToString[int(op)])
-		m.state = HALTED
+		m.Error(fmt.Sprintf("UNIMPLEMENTED INSTRUCTION %q.\n\n", opsToString[int(op)]))
 	}
+
+	m.pc = m.nextProgramCounter(op)
 }
